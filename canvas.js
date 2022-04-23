48b8127dfdfcdf3e448b6e77e6bfe9a691aea8be
LaloFl <defsdiego@gmail.com>
2022-04-23 16:42:55 -0700

    ended

--- canvas.js
+++ canvas.js
@@ -1,27 +1,31 @@
 var canvas = document.getElementById("canvas");
+canvas.focus();
 const ctx = canvas.getContext("2d");
 const canvasSize = canvas.clientWidth;
 ctx.fillStyle = "#252525";
 ctx.fillRect(0, 0, canvasSize, canvasSize);
-let nTiles = 6;
+let nTiles = 8;
 while (canvasSize % nTiles !== 0) {
   nTiles++;
 }
 let tileSize = canvasSize / nTiles;
 let gap = tileSize / 10;
+let maxGap = gap * 7;
 
+let skippedMovement = false;
 let snake = [];
 let usedTiles = [];
 let direction = "down";
 let applePos;
-const drawTile = (x, y, color) => {
-  usedTiles.push({ x, y });
+
+const drawTile = (x, y, color, isApple = false, customGap = gap) => {
+  if (customGap > maxGap) customGap = maxGap;
   ctx.fillStyle = color;
   ctx.fillRect(
-    x * tileSize + gap / 2,
-    y * tileSize + gap / 2,
-    tileSize - gap,
-    tileSize - gap
+    x * tileSize + customGap / 2,
+    y * tileSize + customGap / 2,
+    tileSize - customGap,
+    tileSize - customGap
   );
 };
 
@@ -33,79 +37,121 @@ const tileIsUsed = (x, y) => {
 };
 
 const generateApple = () => {
-  let x = Math.floor(Math.random() * nTiles);
-  let y = Math.floor(Math.random() * nTiles);
+  let x = Math.floor(Math.random() * (nTiles - 1));
+  let y = Math.floor(Math.random() * (nTiles - 1));
   if (tileIsUsed(x, y)) {
     return generateApple();
   }
+
   applePos = { x, y };
-  drawTile(x, y, "#f00");
+  drawTile(x, y, "#f00", true);
 };
 
-const eatApple = () => {};
+const eatApple = () => {
+  generateApple();
+  snake.push({ x: snake[snake.length - 1].x, y: snake[snake.length - 1].y });
+};
 
 const drawSnake = () => {
-  snake.forEach((tile) => {
-    drawTile(tile.x, tile.y, "#0f0");
+  ctx.fillStyle = "#252525";
+  ctx.fillRect(0, 0, canvasSize, canvasSize);
+  drawTile(applePos.x, applePos.y, "#f00", true);
+  snake.forEach((tile, i) => {
+    drawTile(tile.x, tile.y, "#0f0", false, gap + i * 4);
   });
 };
 
-// setInterval(() => {
-//   generateApple();
-// }, 100);
-
+let wait = false;
 canvas.addEventListener("keydown", (e) => {
-  console.log(e.key);
+  if (
+    (e.key !== "ArrowUp" &&
+      e.key !== "ArrowDown" &&
+      e.key !== "ArrowLeft" &&
+      e.key !== "ArrowRight") ||
+    wait
+  )
+    return;
+
+  wait = true;
+  let directionKey = e.key.substring(5, e.key.length).toLowerCase();
+  if (
+    (direction === "right" && directionKey === "left") ||
+    (direction === "left" && directionKey === "right") ||
+    (direction === "up" && directionKey === "down") ||
+    (direction === "down" && directionKey === "up")
+  )
+    return;
+
+  direction = directionKey;
+  skippedMovement = true;
+  wait = false;
+  moveSnake();
 });
 
 const moveSnake = () => {
+  if (skippedMovement === true) {
+    skippedMovement = false;
+    return;
+  }
   let newPos = { x: snake[0].x, y: snake[0].y };
   switch (direction) {
     case "up":
       newPos.y--;
       if (newPos.y < 0) {
-        newPos.y = nTiles - 1;
+        gameOver();
       }
       break;
     case "down":
       newPos.y++;
       if (newPos.y > nTiles - 1) {
-        newPos.y = 0;
+        gameOver();
       }
       break;
     case "left":
       newPos.x--;
       if (newPos.x < 0) {
-        newPos.x = nTiles - 1;
+        gameOver();
       }
       break;
     case "right":
       newPos.x++;
       if (newPos.x > nTiles - 1) {
-        newPos.x = 0;
+        gameOver();
       }
       break;
   }
+
   if (tileIsUsed(newPos.x, newPos.y)) {
-    alert("Game Over");
+    gameOver();
     return;
   }
   snake.unshift(newPos);
+
   if (newPos.x === applePos.x && newPos.y === applePos.y) {
     eatApple();
   }
   snake.pop();
+  usedTiles = snake;
   drawSnake();
 };
 
 const startGame = () => {
   snake = [
-    { x: 1, y: 1 },
-    { x: 1, y: 2 },
     { x: 1, y: 3 },
+    { x: 1, y: 2 },
+    { x: 1, y: 1 },
   ];
+  // usedTiles = snake;
+
   generateApple();
   drawSnake();
 };
 
+const gameOver = () => {
+  alert("Game Over");
+};
+
 startGame();
+setInterval(() => {
+  moveSnake();
+}, 160);
